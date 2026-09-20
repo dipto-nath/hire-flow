dc018a5e6f7b1de8ab1c4ceb66f12ffbab685e32
Dipto Nath <reachdipto@gmail.com>
2026-09-20 11:55:28 +0530

    create test_prep.ts

---
+++ backend/test_prep.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from '@prisma/client';
+import { GoogleGenAI } from '@google/genai';
+import { env } from './src/config/env.js';
+
+async function run() {
+  // Just testing the prompt logic
+}
+run();
